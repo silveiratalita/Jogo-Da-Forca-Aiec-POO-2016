f217128f73ce813feec1a9f796b87e0d133cedd3
Talita Cristina <silveira.tacristina@gmail.com>
2016-06-01 19:58:05 -0300

    Comite de clone, teste

--- src/jogo/da/forca/aiec/poo/unidade/pkg2/JogoDaForca.java
+++ src/jogo/da/forca/aiec/poo/unidade/pkg2/JogoDaForca.java
@@ -18,20 +18,122 @@ public class JogoDaForca {
         {
 
             int totalerros = 0;
+
             boolean b = false;
 
             bemVindo();
 
             PalavraSecreta palavra = new PalavraSecreta();
             System.out.println("Sua dica é: " + palavra.getDica());
+
+            System.out.println("_____");
+            System.out.println("|     |");
+            System.out.println("| ");
+            System.out.println("|");
+            System.out.println("| ______");
+            System.out.println("|/     /|");
+            System.out.println("/_____/");
+            System.out.println("|     |");
+
             EntradaTeclado entrada = new EntradaTeclado();
-            while (totalerros < 4 && !palavra.resolvido()) {
-                System.out.println(palavra);
-                char c = entrada.pegaNovaLetra();
+            while (totalerros < 6 && !palavra.resolvido()) {
+                System.out.println("Palavra: " + palavra);
+
+                char c = entrada.pegaNovaLetra(totalerros);
                 if (!palavra.update(c)) {
                     totalerros = totalerros + 1;
-                    System.out.println("Cuidado você só pode ter no máximo 4 erros e você já errou " + totalerros + " vezes. ");
+                    System.out.println("Cuidado você só pode ter no máximo 6 erros e você já errou " + totalerros + " vezes. ");
+
                 }
+                System.out.println("_________________________________________________________________________");
+                System.out.println("");
+                System.out.println("");
+                switch (totalerros) {
+                    case 0:
+                        System.out.println("______");
+                        System.out.println("|     |");
+                        System.out.println("| ");
+                        System.out.println("|");
+                        System.out.println("|");
+                        System.out.println("| ______");
+                        System.out.println("|/     /|");
+                        System.out.println("/_____/");
+                        System.out.println("|     |");
+
+                        break;
+                    case 1:
+                        System.out.println("_____");
+                        System.out.println("|     |");
+                        System.out.println("|     O");
+                        System.out.println("|");
+                        System.out.println("| ______");
+                        System.out.println("|/     /|");
+                        System.out.println("/_____/");
+                        System.out.println("|     |");
+
+                        break;
+                    case 2:
+                        System.out.println("_____");
+                        System.out.println("|     |");
+                        System.out.println("|     O");
+                        System.out.println("|     |");
+                        System.out.println("| ______");
+                        System.out.println("|/     /|");
+                        System.out.println("/_____/");
+                        System.out.println("|     |");
+
+                        break;
+                    case 3:
+                        System.out.println("_____");
+                        System.out.println("|     |");
+                        System.out.println("|   \\O");
+                        System.out.println("|     |");
+                        System.out.println("| ______");
+                        System.out.println("|/     /|");
+                        System.out.println("/_____/");
+                        System.out.println("|     |");
+
+                        break;
+                    case 4:
+                        System.out.println("_____");
+                        System.out.println("|     |");
+                        System.out.println("|   \\O/");
+                        System.out.println("|     |");
+                        System.out.println("| ______");
+                        System.out.println("|/     /|");
+                        System.out.println("/_____/");
+                        System.out.println("|     |");
+
+                        break;
+                    case 5:
+                        System.out.println("______");
+                        System.out.println("|     |");
+                        System.out.println("|   \\O/");
+                        System.out.println("|     |");
+                        System.out.println("     / ");
+                        System.out.println("| _______");
+                        System.out.println("|/      /|");
+                        System.out.println("/__/___/");
+                        System.out.println("|     |");
+
+                        break;
+                    case 6:
+                        System.out.println("_____");
+                        System.out.println("|     |");
+                        System.out.println("|   \\O/");
+                        System.out.println("|     |");
+                        System.out.println("     /\\");
+                        System.out.println(" |______");
+                        System.out.println("|/     /|");
+                        System.out.println("/_____/");
+                        System.out.println("|     |");
+
+                        break;
+
+                    default:
+                        System.out.println("Numero de erros liberados");
+                }
+
             }
             if (palavra.resolvido()) {
                 System.out.println("Muito bem, você conseguiu");
